828160c4aaedc986cd54f97402245f6bd8c82325
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-04 14:42:58 +0100

    Create method that return appropriate number

--- src/main/java/day1/Main.java
+++ src/main/java/day1/Main.java
@@ -11,8 +11,23 @@ class Main {
 
     // https://adventofcode.com/2020/day/1
     public static void main(String[] args) {
-
+        Main main = new Main();
+        int result = main.getAppropriateNumber();
+        System.out.println(result);
     }
+
+    private int getAppropriateNumber() {
+        var inputNumbers = readAllNumbersFromFile();
+        for (int entryNumber : inputNumbers) {
+            int difference = 2020 - entryNumber;
+            int secondEntryNumber = inputNumbers.stream().filter(n -> n == difference).findFirst().orElse(0);
+            if (secondEntryNumber != 0) {
+                return entryNumber * secondEntryNumber;
+            }
+        }
+        return 0;
+    }
+
 
     private List<Integer> readAllNumbersFromFile() {
         Path path = Path.of("src/main/resources/data.csv");
